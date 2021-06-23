07f92715d391f113145c89d087db20965b51c41e
Alice.Hsieh <Alice.Hsieh@advantech.com.tw>
2021-06-23 13:57:42 +0800

    update: change logger library for file name & line number support

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/lib/pq v1.10.2
+	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.7.1
-	github.com/withmandala/go-log v0.1.0
 	go.mongodb.org/mongo-driver v1.5.3
 )
--- logger.go
+++ logger.go
@@ -1,16 +1,28 @@
 package utility
 
 import (
-	"os"
+	"fmt"
+	"path"
+	"runtime"
 
-	"github.com/withmandala/go-log"
+	"github.com/sirupsen/logrus"
 )
 
 //Logger New
 var Logger = NewLogger()
 
 //NewLogger func
-func NewLogger() *log.Logger {
-	logger := log.New(os.Stderr).WithColor()
+func NewLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetReportCaller(true) // show file name and line number
+	logger.Formatter = &logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			filename := path.Base(f.File)
+			return fmt.Sprintf("%s()", f.Function), fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+	}
+
 	return logger
 }
